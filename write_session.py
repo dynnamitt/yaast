ccafc06c205091462c86427682ff34b8307fbda6
kjetil <kjetil flovild-midtlie>
2020-02-20 14:20:55 +0100

    even more awesome

--- write_session.py
+++ write_session.py
@@ -11,13 +11,15 @@ import logging
 from logging import debug,info,error,warning
 
 logging.basicConfig(level=logging.DEBUG)
+# TODO cleanup format
 
 def main():
 
     if environ.get('AWS_PROFILE'):
         warning("env AWS_PROFILE is set (use -p to override)")
 
-    def_profile = environ['AWS_PROFILE'] if "AWS_PROFILE" in environ else "awsops"
+    def_src_profile = environ['AWS_PROFILE'] if "AWS_PROFILE" in environ else "awsops"
+    def_dest_profile = "default"
 
     parser = argparse.ArgumentParser(
             description='''Rewrites config state in ~/.aws/ files
@@ -25,50 +27,74 @@ def main():
             epilog="More details in README.md file"
             )
 
-    parser.add_argument('-p','--profile',default=def_profile)
+    parser.add_argument('-p','--profile',default=def_src_profile)
+    parser.add_argument('-d','--dest-profile',default=def_dest_profile)
     parser.add_argument('mfacode')
     args = parser.parse_args()
 
-    print(f"Using profile [{args.profile}].")
+    print(f"Selected *source* profile [{args.profile}].")
 
     try:
-        c = Conf(args.profile)
+        source = Conf(args.profile)
     except LookupError as ex:
         error(str(ex))
         exit(1)
     else:
-        print(c.pair)
+        print(source.get_props_dict('''x mfa_serial region
+        aws_secret_access_key aws_access_key_id
+         aws_session_token'''))
+        #print(source.pair[1])
 
 class Conf:
-    '''Fixes some issues found in boto3+awscli, see README'''
+    '''used twice ; once w source profile then w target profile'''
 
     conf_path = Path.home() / ".aws/config"
     cred_path = Path.home() / ".aws/credentials"
 
-    def __init__(self,profile):
+    def __init__(self, profile, must_exist=True):
+
+        self.profile = profile
 
         self.credentials = configparser.ConfigParser()
         self.credentials.read(Conf.cred_path)
         debug(self.credentials.sections())
-        if not profile in self.credentials.sections():
-          raise LookupError(f"The profile '{profile}' not found in {Conf.cred_path}")
 
         self.config = configparser.ConfigParser()
         self.config.read(Conf.conf_path)
         debug(self.config.sections())
-        c_section_name = self.__find_c_section_name(self.config.sections(), profile=profile)
-        if not c_section_name:
-          raise LookupError(f"The profile '{profile}' not found in {Conf.conf_path}")
+
+        if must_exist:
+            self.__error_when_nonexistant()
+
+        c_section_name = self.__find_c_section_name()
 
         self.core_profile = (
             self.config[c_section_name],
-            self.credentials[profile]
+            self.credentials[self.profile]
         )
 
-    @classmethod
-    def __find_c_section_name(self, sections, profile):
-        res = [s for s in sections if re.search(profile,s)]
+    def __error_when_nonexistant(self):
+        if not self.profile in self.credentials.sections():
+          raise LookupError(f"Profile '{self.profile}' not found in {Conf.cred_path}")
+        c_section_name = self.__find_c_section_name()
+        if not c_section_name:
+          raise LookupError(f"Profile '{self.profile}' not found in {Conf.conf_path}")
+
+    def __find_c_section_name(self):
+        res = [s for s in self.config.sections() if re.search(self.profile,s)]
         return res[0] if len(res) else None
+
+    # as dict
+    def get_props_dict(self,props):
+        '''Combine result from both INI files
+        and give credentials precedence'''
+        _props = props if isinstance(props,list) else props.split()
+        debug(_props)
+        config_sect,cred_sect = self.core_profile
+        return dict(
+            [(key,config_sect[key]) for key in config_sect if key in _props] +
+            [(key,cred_sect[key]) for key in cred_sect if key in _props]
+            )
 
     @property
     def pair(self):
