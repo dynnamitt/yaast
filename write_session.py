6c7ff5647f0a8bc46125f68b5b30fd07b030e29e
kjetil <kjetil flovild-midtlie>
2020-02-20 12:47:50 +0100

    progress

--- write_session.py
+++ write_session.py
@@ -1,33 +1,78 @@
 #!/usr/bin/python3
 
-import boto3
+import botocore
+import configparser
 from os import environ
+from sys import exit
+from pathlib import Path
+import re
 import argparse
 import logging
-from logging import info,error,warning
+from logging import debug,info,error,warning
 
 logging.basicConfig(level=logging.DEBUG)
 
+def main():
 
-if environ.get('AWS_PROFILE'):
-    warning("env AWS_PROFILE is set (use -p to override)")
+    if environ.get('AWS_PROFILE'):
+        warning("env AWS_PROFILE is set (use -p to override)")
 
-def_profile = environ['AWS_PROFILE'] if "AWS_PROFILE" in environ else "awsops"
+    def_profile = environ['AWS_PROFILE'] if "AWS_PROFILE" in environ else "awsops"
 
-parser = argparse.ArgumentParser(
-        description='''Rewrites config state in ~/.aws/ files
-        to simplify everyday MFA work''',
-        epilog="More details in README.md file"
+    parser = argparse.ArgumentParser(
+            description='''Rewrites config state in ~/.aws/ files
+            to simplify everyday MFA work''',
+            epilog="More details in README.md file"
+            )
+
+    parser.add_argument('-p','--profile',default=def_profile)
+    parser.add_argument('mfacode')
+    args = parser.parse_args()
+
+    print(f"Using profile [{args.profile}].")
+
+    try:
+        c = Conf(args.profile)
+    except LookupError as ex:
+        error(str(ex))
+        exit(1)
+    else:
+        print(c.pair)
+
+class Conf:
+    '''Fixes some issues found in boto3+awscli, see README'''
+
+    conf_path = Path.home() / ".aws/config"
+    cred_path = Path.home() / ".aws/credentials"
+
+    def __init__(self,profile):
+
+        self.credentials = configparser.ConfigParser()
+        self.credentials.read(Conf.cred_path)
+        debug(self.credentials.sections())
+        if not profile in self.credentials.sections():
+          raise LookupError(f"The profile '{profile}' not found in {Conf.cred_path}")
+
+        self.config = configparser.ConfigParser()
+        self.config.read(Conf.conf_path)
+        debug(self.config.sections())
+        c_section_name = self.__find_c_section_name(self.config.sections(), profile=profile)
+        if not c_section_name:
+          raise LookupError(f"The profile '{profile}' not found in {Conf.conf_path}")
+
+        self.core_profile = (
+            self.config[c_section_name],
+            self.credentials[profile]
         )
 
-parser.add_argument('-p','--profile',default=def_profile)
-parser.add_argument('mfacode')
-args = parser.parse_args()
+    @classmethod
+    def __find_c_section_name(self, sections, profile):
+        res = [s for s in sections if re.search(profile,s)]
+        return res[0] if len(res) else None
 
-print("*" * 60)
-print("This tool will CHANGE STATE by edit of files ~/.aws/{credentials,config} ")
-print("The very state used by this very tool, so DOUBLECHECK your arguments")
-print("=" * 60)
+    @property
+    def pair(self):
+        return self.core_profile
 
-print(f"Using profile [{args.profile}].")
-session = boto3.Session
+if __name__ == "__main__":
+   main()
