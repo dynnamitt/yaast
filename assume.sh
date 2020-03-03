b2ace863be98d6d9a596ec1789cacf10dab84ac8
Kjetil Flovild-Midtlie <kjetil.flovild@gmail.com>
2020-03-03 16:26:01 +0100

    CONSOLE_URL added to bash impl

--- assume.sh
+++ assume.sh
@@ -47,7 +47,7 @@ if [ -z "$AWS_PROFILE" ];then
   usage
 fi
 
-echo 1>&2 "Profile = $AWS_PROFILE"
+# echo 1>&2 "Profile = $AWS_PROFILE"
 
 # required args:
 ROLE_ARN=$(aws --profile $AWS_PROFILE configure get role_arn)
@@ -65,9 +65,30 @@ if [ ! -z "$MFA_SERIAL" ]; then
   _MFA_SERIAL="--serial-number $MFA_SERIAL"
 fi
 
+TMP_CREDS_FILE=/tmp/_Credentials
 
 aws sts assume-role \
   --role-arn "$ROLE_ARN" \
   --role-session-name "$SESS_NAME" \
-  $_EXT_ID $_MFA_SERIAL
+  $_EXT_ID $_MFA_SERIAL > $TMP_CREDS_FILE
+
+
+REQ_SESS=$(jq '{ "sessionId" : .Credentials.AccessKeyId,
+  "sessionKey": .Credentials.SecretAccessKey,
+  "sessionToken": .Credentials.SessionToken}' \
+  < $TMP_CREDS_FILE)
+
+
+REQ_SESS_ENCODED=$(_rawurlencode "$REQ_SESS")
+URL="https://signin.aws.amazon.com/federation?Action=getSigninToken&Session=${REQ_SESS_ENCODED}"
+
+#echo $URL
+SIGNIN_TOKEN=$(curl --silent ${URL} | jq -r '.SigninToken')
+CONSOLE=$(_rawurlencode "https://console.aws.amazon.com/")
+
+CONSOLE_URL="https://signin.aws.amazon.com/federation?Action=login&Issuer=&Destination=${CONSOLE}&SigninToken=${SIGNIN_TOKEN}"
+
+# MERGE it back out .. ready for another code
+jq '{ "Credentials" : .Credentials, "CONSOLE_URL": "'$CONSOLE_URL'"}' < $TMP_CREDS_FILE
+
 
