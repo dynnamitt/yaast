e546a13b9cf9f36bc14acb7310e5cce967d2b829
kjetil midtlie <kjetil.midtlie@gmail.com>
2021-01-26 10:11:51 +0100

    backup-inp commented out since I havent added it so yeah

--- write_session.py
+++ write_session.py
@@ -9,9 +9,9 @@ from sys import exit
 
 import argparse
 import logging
-from logging import debug,info,error,warning
+from logging import info, error, warning
 
-from awsconfigparser import CFile,AWSConfParser
+from awsconfigparser import CFile, AWSConfParser
 
 # FIXME lookup the python-way (setup_tools)
 VER = "0.0.2"
@@ -20,8 +20,9 @@ HOMEPAGE = "example.com"
 
 logging.basicConfig(level=logging.INFO)
 
+
 def main(profile, dest_profile, mfacode):
-    
+
     print(f"Selected *start* profile [{profile}].")
     print(f"         *dest*  profile [{dest_profile}].")
 
@@ -38,7 +39,7 @@ def main(profile, dest_profile, mfacode):
     aws_session = Session(profile=profile)
 
     creds, backup = load_creds(dest_profile)
-    
+
     # useful stuff in botocore:
     scopeConfig = aws_session.get_scoped_config()
 
@@ -60,30 +61,34 @@ def main(profile, dest_profile, mfacode):
     info(f"Downloaded new temp/creds. ID = {r_creds['AccessKeyId']}")
     #info(resp)
 
-    
     creds.set_new_attrs(backup, **attribs_from_raw(r_creds))
 
     edits = creds.save()
     print(f"Wrote edits to file(s) : {[str(fn.path) for fn in edits]}")
 
 
-
-def load_creds(dest_profile:str):
+def load_creds(dest_profile: str):
     """Load creds object and get backup bool"""
 
     creds = AWSConfParser(dest_profile, CFile.CREDS)
 
     backup = False
 
-    if creds.exists and creds.get('aws_session_token') :
-        inp = input(f"Would you like to backup the old '{dest_profile}' session profile? [N/y] ")
-        backup = inp.strip().lower() == 'y'
-    elif creds.exists:
-        print("WARNING: There is an existing (NON SESSION) destination profile.. ")
-        inp = input(f" ..would you like to backup this '{dest_profile}' profile? [Y/n] ")
-        backup = inp.strip().lower() == 'y'
+    # if creds.exists and creds.get('aws_session_token'):
+    #     inp = input(
+    #         f"Would you like to backup the old '{dest_profile}' session profile? [N/y] "
+    #     )
+    #     backup = inp.strip().lower() == 'y'
+    # elif creds.exists:
+    #     print(
+    #         "WARNING: There is an existing (NON SESSION) destination profile.. "
+    #     )
+    #     inp = input(
+    #         f" ..would you like to backup this '{dest_profile}' profile? [Y/n] "
+    #     )
+    #     backup = inp.strip().lower() == 'y'
 
-    return creds,backup
+    return creds, backup
 
 
 def sts_session_token(aws_session, mfacode=None, mfa_serial=None):
@@ -91,22 +96,22 @@ def sts_session_token(aws_session, mfacode=None, mfa_serial=None):
     attributes already part of aws_session isn't passed on to client,
     so we FIX that here"""
     client = aws_session.create_client('sts')
-    return client.get_session_token( SerialNumber=mfa_serial,
-                                    TokenCode=mfacode )
+    return client.get_session_token(SerialNumber=mfa_serial, TokenCode=mfacode)
 
-def attribs_from_raw(raw_credentials) :
+
+def attribs_from_raw(raw_credentials):
     """Transform API response object into coresponding 
        names inside the credentials-file"""
 
     # FIXME OrderedDict?
     return {
-        "__appended_by_script__" : SCRIPT_NAME + f" {VER}",
-        "__homepage__" : HOMEPAGE,
-        "aws_access_key_id" : raw_credentials['AccessKeyId'],
-        "aws_secret_access_key" : raw_credentials['SecretAccessKey'],
-        "aws_session_token" : raw_credentials['SessionToken'],
-        "expiration" : raw_credentials['Expiration']
-        }
+        "__appended_by_script__": SCRIPT_NAME + f" {VER}",
+        "__homepage__": HOMEPAGE,
+        "aws_access_key_id": raw_credentials['AccessKeyId'],
+        "aws_secret_access_key": raw_credentials['SecretAccessKey'],
+        "aws_session_token": raw_credentials['SessionToken'],
+        "expiration": raw_credentials['Expiration']
+    }
 
 
 if __name__ == "__main__":
@@ -114,18 +119,24 @@ if __name__ == "__main__":
     if environ.get('AWS_PROFILE'):
         warning("env AWS_PROFILE is set (use -p to override)")
 
-    def_src_profile = environ.get('AWS_PROFILE',default="awsops")
+    def_src_profile = environ.get('AWS_PROFILE', default="awsops")
     def_dest_profile = "default"
 
-    parser = argparse.ArgumentParser(
-            description=__doc__,
-            epilog="More details in README.md file"
-            )
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     epilog="More details in README.md file")
 
-    parser.add_argument('-p', '--profile', default=def_src_profile,
-                        help = f'The profile w/ mfa info and start credentials. "{def_src_profile}" when unset')
-    parser.add_argument('-d','--dest-profile',default=def_dest_profile,
-                        help = f'Dest profile to write to. "{def_dest_profile}" when unset')
+    parser.add_argument(
+        '-p',
+        '--profile',
+        default=def_src_profile,
+        help=
+        f'The profile w/ mfa info and start credentials. "{def_src_profile}" when unset'
+    )
+    parser.add_argument(
+        '-d',
+        '--dest-profile',
+        default=def_dest_profile,
+        help=f'Dest profile to write to. "{def_dest_profile}" when unset')
     parser.add_argument('mfacode')
 
     args = parser.parse_args()
